be1b98487b1bc7caa94f566eddd3944369b11956
artienegronjr <artienegronjr@gmail.com>
2021-07-26 14:08:06 -0400

    Added initial input validation for time pickers.

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -10,6 +10,93 @@ export default function Calculator() {
   const [bedTime, setBedTime] = React.useState(null);
   const [endTime, setEndTime] = React.useState(null);
 
+  const validateSubmission = (e) => {
+    e.preventDefault(); //Prevents button from submitting form
+
+    let isValid = true;
+
+    isValid = validateStartTime();
+    if (!isValid) {
+      return isValid;
+    }
+
+    isValid = validateEndTime();
+    if (!isValid) {
+      return isValid;
+    }
+
+    isValid = validateBedTime();
+    if (!isValid) {
+      return isValid;
+    }
+
+    return isValid;
+  }
+
+  const validateStartTime = () => {
+    let isValid = true;
+
+    if (startTime === null){
+      isValid = false;
+      return isValid;
+    }
+
+    //Start time should be between 5pm and 3am
+    if (startTime.getHours() >= 4 && startTime.getHours() <= 16) {
+      isValid = false;
+      return isValid;
+    }
+
+    return isValid;
+  }
+
+  const validateBedTime = () => {
+    let isValid = true;
+
+    if (bedTime === null){
+      isValid = false;
+      return isValid;
+    }
+
+    //Bed time must be after start time
+    if (bedTime.getHours() < startTime.getHours()){
+      isValid = false;
+      return isValid;
+    }
+
+    //Bed time must be before end time
+    if (bedTime.getHours() > endTime.getHours()){
+      isValid = false;
+      return isValid;
+    }
+
+    return isValid;
+  }
+
+  //Need to revisit. Not working as expected...
+  const validateEndTime = () => {
+    let isValid = true;
+
+    if (endTime === null){
+      isValid = false;
+      return isValid;
+    }
+
+    //End time must be after start time
+    if (endTime.getHours() <= startTime.getHours()){
+      isValid = false;
+      return isValid;
+    }
+
+    //End time cannot be after 4am
+    if (endTime.getHours() > 4){
+      isValid = false;
+      return isValid;
+    }
+
+    return isValid;
+  }
+
   return (
     <div className="Calculator">
       <LocalizationProvider dateAdapter={AdapterDateFns}>
@@ -38,7 +125,7 @@ export default function Calculator() {
           }}
           renderInput={(params) => <TextField {...params} />}
         />
-        <button>Calculate</button>
+        <button onClick={validateSubmission}>Calculate</button>
       </form>
       </LocalizationProvider>
     </div>
